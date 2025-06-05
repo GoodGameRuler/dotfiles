f71a0f0cf191f503fd24e9fdded3b42743bf9a57
Udit <udit.samant@gmail.com>
2025-06-05 22:22:27 +1000

    Add scripts

---
+++ Scripts/tmux.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+default="default"                               # session name
+
+# Attach if session exists
+if tmux has-session -t "$default" 2>/dev/null; then
+    tmux attach -t "$default"
+    exit
+fi
+
+# Set-up from scratch
+tmux new-session  -d  -s "$default"             # window 0, pane 0
+tmux send-keys    -t "$default":0.0 "jtm" C-m
+
+tmux split-window -h  -t "$default":0.0         # pane 1 (right)
+tmux split-window -v -b -t "$default":0.1       # pane 2 (above pane 1)
+
+tmux send-keys    -t "$default":0.1 "fastfetch" C-m
+tmux send-keys    -t "$default":0.2 "pipes-rs" C-m
+
+tmux attach       -t "$default"
