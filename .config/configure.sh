894af8c939cb74f09c65056a75144931769d9d84
Udit <udit.samant@gmail.com>
2025-06-16 01:34:08 +1000

    Add tmux qol changes and fill out configure sh for arch

--- .config/configure.sh
+++ .config/configure.sh
@@ -9,9 +9,11 @@ afx install
 yay -S hyprcursor-git hyprgraphics-git hypridle-git hyprland-git hyprland-protocols-git hyprland-qt-support-git hyprland-qtutils-git hyprlang-git hyprlock-git hyprpaper-git hyprsunset-git hyprutils-git hyprwayland-scanner-git xdg-desktop-portal-hyprland-git
 yay -S mako swayosd waybar playerctl
 
-yay -S bob nvm github-cli fd zsh-auto-suggestions zsh-syntax-highlight
+yay -S bob nvm github-cli fd zsh-auto-suggestions zsh-syntax-highlight ranger
 
 nvm install nodejs
 bob install nightly
 
 yay -S rust make ninja cmake go python3 python3-pip fzf
+yay -S texlive texlive-basic texlive-latexrecommended texlive-latexextra texlive-fontsrecommended texlive-bibtexextra texlive-mathscience
+
