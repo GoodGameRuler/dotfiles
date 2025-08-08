037e2cf41cc9772e5a00cedd40a4ac7f15cc4d3f
Udit <udit.samant@gmail.com>
2025-08-09 00:41:36 +1000

    Add changes to script

--- Scripts/journal.sh
+++ Scripts/journal.sh
@@ -11,14 +11,100 @@
 NOTES=/home/udit/OrgNotes
 CACHE=/home/udit/.cache/jtm
 
-ADMIN=/admin
+ADMIN=admin
 
 function jtm() {
-    mkdir -p $CACHE
-    ( git -C "$NOTES" pull >> "$CACHE/git_log" 2>&1 & )
-    FILE="$NOTES/journal/$ADMIN/$(date +%Y-%m-%d).md";
-    if [[ ! -f $FILE ]]; then 
-        cp "$NOTES/journal/$ADMIN/template.md" "$FILE"
+    local arg_date=""
+    local next=false
+    local prev=false
+
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            -d|--date)
+                arg_date="$2"
+                shift 2
+                ;;
+            -n|--next)
+                next=true
+                shift
+                ;;
+            -p|--prev)
+                prev=true
+                shift
+                ;;
+            *)
+                echo "Usage: jtm [-n|--next] [-p|--prev] [-d|--date YYYY-MM-DD]"
+                return 1
+                ;;
+        esac
+    done
+
+    local ref_date=${arg_date:-$(date +%Y-%m-%d)}
+
+    if $next; then
+        ref_date=$(date -d "$ref_date +1 day" +%Y-%m-%d)
+    elif $prev; then
+        ref_date=$(date -d "$ref_date -1 day" +%Y-%m-%d)
     fi
-    nvim "$FILE"
+
+    mkdir -p "$CACHE"
+    ( git -C "$NOTES" pull >> "$CACHE/git_log" 2>&1 & )
+
+    local file="$NOTES/journal/$ADMIN/daily/$ref_date.md"
+    if [[ ! -f $file ]]; then
+        nvim -c "0r $NOTES/journal/$ADMIN/daily/template.md" "$file"
+    else
+        nvim "$file"
+    fi
 }
+
+function jtw() {
+    local arg_date=""
+    local next=false
+    local prev=false
+
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            -d|--date)
+                arg_date="$2"
+                shift 2
+                ;;
+            -n|--next)
+                next=true
+                shift
+                ;;
+            -p|--prev)
+                prev=true
+                shift
+                ;;
+            *)
+                echo "Usage: jtw [-n|--next] [-p|--prev] [-d|--date YYYY-MM-DD]"
+                return 1
+                ;;
+        esac
+    done
+
+    local ref_date=${arg_date:-$(date +%Y-%m-%d)}
+    local sunday=$(date -d "$ref_date last sunday" +%Y-%m-%d)
+
+    if $next; then
+        sunday=$(date -d "$sunday +7 days" +%Y-%m-%d)
+    elif $prev; then
+        sunday=$(date -d "$sunday -7 days" +%Y-%m-%d)
+    fi
+
+    mkdir -p "$CACHE"
+    ( git -C "$NOTES" pull >> "$CACHE/git_log" 2>&1 & )
+
+    local file="$NOTES/journal/$ADMIN/weekly/$sunday.md"
+    if [[ ! -f $file ]]; then
+        nvim -c "0r $NOTES/journal/$ADMIN/weekly/template.md" "$file"
+    else
+        nvim "$file"
+    fi
+}
+
+alias jtg='rg --color=always --line-number --no-heading --smart-case "${*:-}" "$NOTES/journal" \
+  | fzf --ansi --delimiter : \
+        --preview "bat --color=always {1} --highlight-line {2}" \
+        --bind "enter:become(nvim {1} +{2})"'
